072dd9ac43fa3c453d10e79cbcd0679a17eacece
Dominique B <domi_burnand@yahoo.de>
2020-11-12 10:16:38 +0100

    Version: 0.0.10 - Append build-number to image tag

--- build-tags.sh
+++ build-tags.sh
@@ -11,10 +11,10 @@ function join_by { local IFS="$1"; shift; echo "$*"; }
 # Get version from python package
 if [ "$1" == --py-module ]; then
   if [ -z "$2" ]; then
-    VERSION="$(python -c 'import version; print version.__version__')"
+    VERSION="$(python3 -c 'import version; print(version.__version__)')"
   else
     PY_MODULE="$2"
-    VERSION="$(python -c 'import '$PY_MODULE'; print '$PY_MODULE'.__version__')"
+    VERSION="$(python3 -c 'import '$PY_MODULE'; print('$PY_MODULE'.__version__)')"
   fi
   if [ $? -ne 0 ]; then
     echo "ERROR: Was not able to get version from python module"
@@ -45,6 +45,7 @@ GIT_BRANCH=$(git symbolic-ref --short HEAD)
 
 echo "VERSION: ${VERSION}"
 echo "DRONE_COMMIT_BRANCH: ${DRONE_COMMIT_BRANCH}"
+echo "DRONE_BUILD_NUMBER: ${DRONE_BUILD_NUMBER}"
 echo "GIT_BRANCH: " ${GIT_BRANCH}
 echo "DRONE_COMMIT_SHA: ${DRONE_COMMIT_SHA:0:7}"
 
@@ -91,7 +92,7 @@ fi
 if [ "${INCLUDE_FEATURE_TAG}" == "true" ]; then
   CONVERTED_BRANCH=${GIT_BRANCH//\//\_}
   echo "Writing feature style tag" $CONVERTED_BRANCH
-  TAGS+=("${VERSION}-${CONVERTED_BRANCH}.${DRONE_COMMIT_SHA:0:7}")
+  TAGS+=("${VERSION}-${CONVERTED_BRANCH}.${DRONE_COMMIT_SHA:0:7}-${DRONE_BUILD_NUMBER}")
 fi
 
 echo "Writing tags to .tags file:"
